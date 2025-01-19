bcace695e2a329bc8f1b89bf62cf262be0aec75a
gabriel <gabrielscolen@gmail.com>
2025-01-19 18:00:45 -0300

    feat: add new commit
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/com/colen/consulta_cep/controller/ConsultaCepControllerImpl.java
@@ -0,0 +1,12 @@
+package com.colen.consulta_cep.controller;
+
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConsultaCepControllerImpl {
+
+    @Mapping
+
+
+}
